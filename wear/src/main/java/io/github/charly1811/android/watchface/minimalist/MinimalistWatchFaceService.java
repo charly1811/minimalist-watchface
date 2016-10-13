745beadb428faf6dd6840b641c24f4ab4bc49e65
Charles-Eugene Loubao <charlesloubao95@gmail.com>
2016-10-13 16:01:16 -0400

    - Time and date are displayed - The background is drawn

--- wear/src/main/java/io/github/charly1811/android/watchface/minimalist/MinimalistWatchFaceService.java
+++ wear/src/main/java/io/github/charly1811/android/watchface/minimalist/MinimalistWatchFaceService.java
@@ -6,7 +6,11 @@ import android.graphics.Color;
 import android.graphics.Rect;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.text.TextPaint;
+import android.util.TypedValue;
 import android.view.SurfaceHolder;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * On Android Wear Watch Face is implemented as a service.
@@ -27,15 +31,47 @@ public class MinimalistWatchFaceService extends CanvasWatchFaceService {
      */
     public class MinimalWatchFaceEngine extends Engine {
 
+        private long time;
+        private SimpleDateFormat dateFormat;
+        private SimpleDateFormat timeFormat;
+
         private TextPaint timePaint;
+        private TextPaint datePaint;
+        private int backgroundColor;
 
         // Called when the watch face service is created for the first time
         // We will initialize our drawing components here
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
+            time = System.currentTimeMillis();
+
+            // We setup the time and date formatter
+            dateFormat = new SimpleDateFormat("EEEE MMMM dd", Locale.getDefault());
+            timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+            // The time paint
             timePaint = new TextPaint();
             timePaint.setColor(Color.WHITE);
+            timePaint.setAntiAlias(true);
+            timePaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 36, getResources().getDisplayMetrics()));
+
+            // The date paint
+            datePaint = new TextPaint();
+            datePaint.setColor(Color.WHITE);
+            datePaint.setAntiAlias(true);
+            datePaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14, getResources().getDisplayMetrics()));
+
+            // The background color (I am using my favorite shade of Blue Grey here .
+            backgroundColor = Color.parseColor("#607D8B");
+        }
+
+        // Called every minutes
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            time = System.currentTimeMillis();
+            invalidate();
         }
 
         @Override
@@ -43,15 +79,31 @@ public class MinimalistWatchFaceService extends CanvasWatchFaceService {
             super.onDraw(canvas, bounds);
             // We draw the watch face here
 
-            String text = String.valueOf(System.currentTimeMillis());
+            Rect timeBounds = new Rect();
+            String timeText = timeFormat.format(time);
+            int timeX;
+            int timeY;
 
-            Rect textBounds = new Rect();
-            timePaint.getTextBounds(text, 0, text.length(), textBounds);
+            String dateText = dateFormat.format(time);
+            Rect dateBounds = new Rect();
+            int dateX;
+            int dateY;
 
-            int textX = Math.abs(bounds.centerX() - textBounds.centerX());
-            int textY = Math.abs(bounds.centerY() - textBounds.centerY());
+            timePaint.getTextBounds(timeText, 0, timeText.length(), timeBounds);
+            timeX = Math.abs(bounds.centerX() - timeBounds.centerX());
+            timeY = Math.round((Math.abs(bounds.centerY())) - (bounds.height() * 0.02f));
 
-            canvas.drawText(text, textX, textY, timePaint);
+            datePaint.getTextBounds(dateText, 0, dateText.length(), dateBounds);
+            dateX = Math.abs(bounds.centerX() - dateBounds.centerX());
+            dateY = Math.round((bounds.centerY() + dateBounds.height()) + (bounds.height() * 0.02f));
+
+            // We draw the background color
+            canvas.drawColor(backgroundColor);
+
+            // We draw the date and the time
+            canvas.drawText(timeText, timeX, timeY, timePaint);
+            canvas.drawText(dateText, dateX, dateY, datePaint);
+
         }
     }
 }
